e9020f45d40c015bd2d3507638312d1d061cbe7b
yametdev0111 <yametdev0111@gmail.com>
2024-06-11 23:24:21 +0900

    fixed: endpoint

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   output: 'standalone',
   env: {
     // SERVER_ENDPOINT: 'http://localhost:8000',
-    SERVER_ENDPOINT: 'https://seeadback.onrender.com',
+    SERVER_ENDPOINT: 'https://interviewback.onrender.com',
   },
 };
 
